c379902c5172d6a034aa2893df50af0e79a5dd68
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-08-18 21:28:16 -0300

    exercicios for

--- aula4.php
+++ aula4.php
@@ -35,3 +35,32 @@ for($numero = 5, $fatorial = 1; $numero > 0; $numero--) {
 }
 
 echo $fatorial;
+
+echo "<br>";
+
+/**
+ * calcular e exibir a tabuada do 5 utilizando laço for.
+ * resultado esperado:
+ * 5x1 = 5
+ * 5x2 = 10
+ * ...
+ * 5x10 = 50
+ */
+
+
+echo "<br>";
+
+/**
+ * calcular e exibir todos os numeros pares de 2 até 50.
+ * sem pula de 2 em 2 no for;
+ * 
+ * dica: utilizar formula do numeros pares.
+ */
+
+echo "<br>";
+
+/**
+ * calcular e exibir os 5 primeiros numeros primos.
+ * 
+ * dica: utilizar formula do numeros pares.
+ */
